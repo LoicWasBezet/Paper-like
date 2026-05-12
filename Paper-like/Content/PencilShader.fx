737504eca48e0752e0da1af7d0ccb5bec7965c3c
Loic van Asten <l.s.asten@student.tudelft.nl>
2026-05-12 16:00:29 +0200

    begin

---
+++ Paper-like/Content/PencilShader.fx
@@ -0,0 +1,71 @@
+﻿#if OPENGL
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+
+struct VertexShaderInput
+{
+    // Geometry Data (From Buffer A)
+    float4 LocalPos : POSITION0;
+    float2 Position : POSITION1;
+    float2 TexCoord : TEXCOORD0;
+
+    // Instance Data (From Buffer B)
+    // Must match the VertexDeclaration we defined in C# (BlendWeight 0-3)
+    float4 InstanceColor : COLOR1;
+};
+
+struct VertexShaderOutput
+{
+    float4 Position : SV_POSITION;
+    float2 TexCoord : TEXCOORD0;
+    float4 Color : COLOR0;
+};
+
+VertexShaderOutput MainVS(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+
+    // Reconstruct the matrix from the 4 rows
+    output.Color = input.InstanceColor;
+    output.Position = float4(input.LocalPos.xy + input.Position, 0, 1);
+
+    output.TexCoord = input.TexCoord;
+
+    return output;
+}
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+    
+    float2 coords = (input.TexCoord - float2(1, 1) * 0.5f) * 2;
+    float rSqr = coords.x * coords.x + coords.y * coords.y;
+    if (rSqr < 1 && input.Color.a > 0.1f)
+    {
+        //input.Color.a = (0.01f / rSqr);
+        input.Color.a = (1 - sqrt(sqrt(rSqr)) + 0.03f / rSqr - 0.03f * 0.2f) * 0.2f;
+        if (input.Color.a > 1)
+        {
+            input.Color.rgb *= input.Color.a;
+
+        }
+        return input.Color; // Render red for test
+    }
+    discard;
+    return float4(1, 0, 1, 1);
+
+}
+
+technique Instancing
+{
+    pass P0
+    {
+        VertexShader = compile VS_SHADERMODEL MainVS();
+        PixelShader = compile PS_SHADERMODEL MainPS();
+    }
+};
--- Paper-like/Game1.cs
+++ Paper-like/Game1.cs
@@ -1,33 +1,101 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+using System;
 
 namespace Paper_like
 {
+    public struct InstanceData : IVertexType
+    {
+        public Vector4 color;
+        public Vector2 position;
+
+        public static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Vector4, VertexElementUsage.Color, 1),
+            new VertexElement(16, VertexElementFormat.Vector2, VertexElementUsage.Position, 1)
+        );
+
+        VertexDeclaration IVertexType.VertexDeclaration => VertexDeclaration;
+
+        public InstanceData(Vector4 color, Vector2 position)
+        {
+            this.color = color;
+            this.position = position;
+        }
+    }
     public class Game1 : Game
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        RenderTarget2D screen;
+        Effect mainShader;
+
+        DynamicVertexBuffer instanceBuffer;
+        VertexBuffer geometryBuffer;
+        InstanceData[] instances;
+        int maxPointCount = 1000;
+        float dotSize = 0.04f;
+
+        Random rnd = new Random();
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            graphics.ApplyChanges();
+            graphics.PreferredBackBufferWidth = GraphicsDevice.Adapter.CurrentDisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsDevice.Adapter.CurrentDisplayMode.Height;
+            graphics.ToggleFullScreen();
+            //graphics.SynchronizeWithVerticalRetrace = false;
+            //IsFixedTimeStep = false;
+            graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            mainShader = Content.Load<Effect>("PencilShader");
+            //mainShader.Parameters["aspectRatio"].SetValue(graphics.PreferredBackBufferWidth / (float)graphics.PreferredBackBufferHeight);
             // TODO: use this.Content to load your game content here
+            screen = new RenderTarget2D(GraphicsDevice, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight, false, SurfaceFormat.Vector4, DepthFormat.None);//RenderTargetUsage.PreserveContents
+
+
+            short[] indices = new short[] { 0, 1, 2, 2, 1, 3 };
+
+
+            IndexBuffer indexBuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, indices.Length, BufferUsage.WriteOnly);
+            indexBuffer.SetData(indices);
+
+            GraphicsDevice.Indices = indexBuffer;
+
+            instances = new InstanceData[maxPointCount];
+
+            instanceBuffer = new DynamicVertexBuffer(GraphicsDevice, InstanceData.VertexDeclaration, maxPointCount, BufferUsage.WriteOnly);
+
+
+            float XpixelsPerYpixel = ((float)GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width) / ((float)GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height);
+            VertexPositionTexture[] vertices = new VertexPositionTexture[]
+            {
+                new VertexPositionTexture(new Vector3(-1, XpixelsPerYpixel, 0)*dotSize/2, new Vector2(0, 0)),
+                new VertexPositionTexture(new Vector3(1, XpixelsPerYpixel, 0)*dotSize/2, new Vector2(1, 0)),
+                new VertexPositionTexture(new Vector3(-1, -XpixelsPerYpixel, 0)*dotSize/2, new Vector2(0, 1)),
+
+                new VertexPositionTexture(new Vector3(1, -XpixelsPerYpixel, 0)*dotSize/2, new Vector2(1, 1))
+            };
+            geometryBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionTexture), 4, BufferUsage.WriteOnly);
+            geometryBuffer.SetData(vertices);
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +103,54 @@ namespace Paper_like
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            for (int i = 0; i < maxPointCount; i++)
+            {
+                instances[i].position = new Vector2(rnd.NextSingle()*1000,rnd.NextSingle()*1000);
+                instances[i].color = new Vector4(rnd.NextSingle()*255,rnd.NextSingle()*255,rnd.NextSingle()*255,1);
+            }
+            instanceBuffer.SetData(instances);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(new Color(1.0f / 255, 3.0f / 255, 20.0f / 255, 1));
+            GraphicsDevice.BlendState = BlendState.Additive;
+            GraphicsDevice.DepthStencilState = DepthStencilState.None;
 
-            // TODO: Add your drawing code here
+            GraphicsDevice.SetVertexBuffers(
+                new VertexBufferBinding(geometryBuffer, 0, 0),
+                new VertexBufferBinding(instanceBuffer, 0, 1)
+            );
+
+
+
+            foreach (EffectPass pass in mainShader.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawInstancedPrimitives(
+                    PrimitiveType.TriangleList, 0, 0, 2, 0
+                );
+            }
+            if (rnd.Next(0, 100) == 0)
+            {
+                Debug.WriteLine(1 / gameTime.ElapsedGameTime.TotalSeconds);
+            }
+
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+            mainShader.CurrentTechnique.Passes[0].Apply();
+            spriteBatch.Draw(
+                texture: screen,
+                position: Vector2.Zero,
+                sourceRectangle: null,
+                color: Color.White,//Color.RosyBrown
+                rotation: 0,
+                origin: Vector2.Zero,
+                scale: Vector2.One,
+                effects: SpriteEffects.None,
+                layerDepth: 0f);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
